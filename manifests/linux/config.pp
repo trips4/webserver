2cd5712a0bf6e243f1fd6ee03750d5a4c80e5625
trips4 <dennis.triplo@perforce.com>
2025-07-31 11:24:42 -0500

    refactor per review

--- manifests/linux/config.pp
+++ manifests/linux/config.pp
@@ -5,11 +5,13 @@
 class webserver::linux::config (
   String $http_port,
 ) {
-  $current_http_port = $facts['webserver_http_port']
-
-  exec { "update /etc/apache2/ports.conf to Listen on ${http_port}":
-    command  => "sed -i 's/^Listen ${current_http_port}$/Listen ${http_port}/' /etc/apache2/ports.conf",
-    unless   => "grep -q '^Listen ${http_port}$' /etc/apache2/ports.conf",
-    provider => 'shell',
+  file_line { '/etc/apache2/ports.conf':
+    ensure  => present,
+    path    => '/etc/apache2/ports.conf',
+    line    => "Listen ${http_port}",
+    match   => "^Listen\\s+",
+    replace => true,
+    require => Class['webserver::linux::package'],
+    notify  => Class['webserver::service'],
   }
 }
--- spec/classes/linux/config_spec.rb
+++ spec/classes/linux/config_spec.rb
@@ -3,13 +3,9 @@
 require 'spec_helper'
 
 describe 'webserver::linux::config' do
-  on_supported_os.each do |os, os_facts|
+  on_supported_os.select { |_os, facts| facts[:os]['name'] == 'Ubuntu' }.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) do
-        os_facts.merge({
-                         webserver_http_port: '80',
-                       })
-      end
+      let(:facts) { os_facts }
 
       let(:params) do
         {
@@ -18,15 +14,6 @@ describe 'webserver::linux::config' do
       end
 
       it { is_expected.to compile.with_all_deps }
-
-      it do
-        is_expected.to contain_exec('update /etc/apache2/ports.conf to Listen on 8080')
-          .with(
-            command:  "sed -i 's/^Listen 80$/Listen 8080/' /etc/apache2/ports.conf",
-            unless:   "grep -q '^Listen 8080$' /etc/apache2/ports.conf",
-            provider: 'shell',
-          )
-      end
     end
   end
 end
