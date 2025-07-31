d90283b9499b4350d98b41dbfc83f32a70fb1bbf
trips4 <dennis.triplo@perforce.com>
2025-07-31 15:42:30 -0500

    refactor per review

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,19 +11,11 @@ class webserver (
     include webserver::windows::package
     class { 'webserver::windows::config': http_port => $http_port, }
     include webserver::service
-
-    Class['webserver::windows::package']
-    -> Class['webserver::windows::config']
-    ~> Class['webserver::service']
   }
   elsif $facts['os']['name'] == 'Ubuntu' {
     include webserver::linux::package
     class { 'webserver::linux::config': http_port => $http_port, }
     include webserver::service
-
-    Class['webserver::linux::package']
-    -> Class['webserver::linux::config']
-    ~> Class['webserver::service']
   }
   else {
     fail("Unsupported OS : ${facts['os']['name']}")
