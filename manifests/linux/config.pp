29ea25983d0489cad4d3fcd25d038ab416194968
trips4 <dennis.triplo@perforce.com>
2025-08-11 10:58:03 -0500

    added contains to fix tests

--- manifests/linux/config.pp
+++ manifests/linux/config.pp
@@ -5,6 +5,8 @@
 class webserver::linux::config (
   String $http_port,
 ) {
+  contain webserver::linux::package
+  contain webserver::service
   file_line { '/etc/apache2/ports.conf':
     ensure  => present,
     path    => '/etc/apache2/ports.conf',
