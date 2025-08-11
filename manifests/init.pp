10041408c84fa511b97308c43dec906895d899e8
trips4 <dennis.triplo@perforce.com>
2025-08-11 11:41:44 -0500

    updating logic in init to case statements

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,10 @@ class webserver (
 ) {
   case $facts['os']['family'].downcase {
     'windows', 'debian': {
-      $mod_fam = "${module_name}::${facts['os']['family'].downcase}"
+      $mod_kern = "${module_name}::${facts['kernel'].downcase}"
 
-      include "${mod_fam}::package"
-      class { "${mod_fam}::config":
+      include "${mod_kern}::package"
+      class { "${mod_kern}::config":
         http_port => $http_port,
       }
       include webserver::service
